6958f49ab43e3f8dbbb4bf0fd11b2d0f2ea43a72
krid-sdnera <krid-sdnera@users.noreply.github.com>
2021-10-27 22:22:44 +1100

    feat: Add first-aid page route

---
+++ src/pages/first-aid.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col xs="12" sm="6">
+      <v-card>
+        <v-card-title>First Aid</v-card-title>
+
+        <v-card-text> plz dont hurt your self </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  layout: "default",
+};
+</script>
