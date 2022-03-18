92acd954ffef301da9d67e09909c2e4b1c9a45a9
Dirk Arends <dirk@arends.com.au>
2022-03-19 10:46:49 +1100

    wip

--- src/server/api/record.ts
+++ src/server/api/record.ts
@@ -1,18 +1,30 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { EventLog, EventLogRawInput, GraphQLBasic } from "~/types";
-import { body2Data, simpleGraphQL } from ".";
+import {
+  ConnectableEntity,
+  EventLog,
+  EventLogPersisted,
+  EventLogRaw,
+  EventLogRawInput,
+  ValidEventLogTypes,
+} from "~/types";
+import { makeError, nuxtConfig, simpleAllGraphQL } from ".";
+import uuid4 from "uuid4";
 
 // Converts type strings from APP types to CMS types.
 const entityTypeMapping = {
-  admin: "Admin",
   patrol: "Patrol",
   stunt: "Stunt",
-  monsterHuntMonster: "MonsterHuntMonster",
+  monstemonGo: "MonstemonGo",
 };
+
 export default async (req: IncomingMessage, res: ServerResponse) => {
+  // Only allow POST requests.
+  if (req.method !== "POST") {
+    res.statusCode = 405;
+    return `405 Method Not Allowed`;
+  }
+
   // Parse request body.
-  // const testData = await useBody(req);
-  // let inputData: Partial<EventLog> = await body2Data<EventLog>(req);
   let inputData: Partial<EventLog> = {};
 
   if (process.env.NETLIFY === "true") {
@@ -32,210 +44,141 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
       });
     });
   }
-  res.statusCode = 200;
+  console.log(inputData);
 
-  return {
-    data: {
-      eventLog: inputData,
-    },
-  };
-  // // console.log("testData", testData);
-  // console.log("inputData", inputData);
+  // Confirm there is a valid deduplication ID.
+  if (!inputData.deduplicationId || !uuid4.valid(inputData.deduplicationId)) {
+    const error = `Event Log deduplicationId is invalid ${inputData.type}`;
+    return makeError(res, 400, error);
+  }
 
-  // // TODO: Move this logic to a common function to be validated in the UI too.
-  // // Confirm there is a valid deduplication ID.
-  // if (!inputData.deduplicationId || !uuid4.valid(inputData.deduplicationId)) {
-  //   const error = `Event Log deduplicationId is invalid ${inputData.type}`;
-  //   return makeError(res, 400, error);
-  // }
+  // Confirm there is a valid version.
+  if (!inputData.version) {
+    const error = `Event Log version is invalid ${inputData.type}`;
+    return makeError(res, 400, error);
+  }
 
-  // // Confirm there is a version.
-  // if (!inputData.version) {
-  //   const error = `Event Log version is invalid ${inputData.type}`;
-  //   return makeError(res, 400, error);
-  // }
+  // Confirm there is a valid hash.
+  if (!inputData.hash) {
+    const error = `Event Log hash is invalid ${inputData.type}`;
+    return makeError(res, 400, error);
+  }
 
-  // // Confirm there is a hash.
-  // if (!inputData.hash) {
-  //   const error = `Event Log hash is invalid ${inputData.type}`;
-  //   return makeError(res, 400, error);
-  // }
+  // Confirm the event name matches what we expect.
+  const expectedEventName = nuxtConfig.RUNTIME_CONFIG.public.eventName;
+  if (!inputData.eventName || inputData.eventName !== expectedEventName) {
+    const error = `Event Log name is invalid ${inputData.type}`;
+    return makeError(res, 400, error);
+  }
 
-  // // Confirm the event name matches what we expect.
-  // const expectedEventName = nuxtConfig.RUNTIME_CONFIG.public.eventName;
-  // if (!inputData.eventName || inputData.eventName !== expectedEventName) {
-  //   const error = `Event Log name is invalid ${inputData.type}`;
-  //   return makeError(res, 400, error);
-  // }
+  // Confirm the log type matches a valid type.
+  if (!inputData.type || !ValidEventLogTypes.includes(inputData.type)) {
+    const error = `Event Log type is invalid ${inputData.type}`;
+    return makeError(res, 400, error);
+  }
 
-  // // Confirm the log type matches a valid type.
-  // if (!inputData.type || !ValidEventLogTypes.includes(inputData.type)) {
-  //   const error = `Event Log type is invalid ${inputData.type}`;
-  //   return makeError(res, 400, error);
-  // }
+  // Recording Entity
+  let recordingEntitySearchable: string | null = null;
+  let recordingEntity: ConnectableEntity | null = null;
+  if (inputData.recordingEntity) {
+    const recordingEntityType =
+      entityTypeMapping[inputData.recordingEntity?._type ?? ""];
 
-  // // Recording Entity
-  // let recordingEntity: ConnectableEntity | null = null;
-  // let recordingEntitySearchable: string | null = null;
-  // if (inputData.recordingEntity) {
-  //   const recordingEntityType =
-  //     entityTypeMapping[inputData.recordingEntity?._type ?? ""];
-
-  //   if (!recordingEntityType) {
-  //     const error = `Recording Entity Type Invalid ${inputData.recordingEntity?._type}`;
-  //     return makeError(res, 400, error);
-  //   }
-
-  //   recordingEntitySearchable = `${inputData.recordingEntity._type}:${inputData.recordingEntity.id}`;
-  //   recordingEntity = {
-  //     connect: {
-  //       [recordingEntityType]: {
-  //         id: inputData.recordingEntity.id,
-  //       },
-  //     },
-  //   };
-  // }
-
-  // // Referenced Entity
-  // let referencedEntity: ConnectableEntity | null = null;
-  // let referencedEntitySearchable: string | null = null;
-  // if (inputData.referencedEntity) {
-  //   const referencedEntityType =
-  //     entityTypeMapping[inputData.referencedEntity?._type ?? ""];
-
-  //   if (!referencedEntityType) {
-  //     const error = `Referenced Entity Type Invalid ${inputData.referencedEntity?._type}`;
-  //     return makeError(res, 400, error);
-  //   }
-
-  //   referencedEntitySearchable = `${inputData.referencedEntity._type}:${inputData.referencedEntity.id}`;
-  //   referencedEntity = {
-  //     connect: {
-  //       [referencedEntityType]: {
-  //         id: inputData.referencedEntity.id,
-  //       },
-  //     },
-  //   };
-  // }
-
-  // const logData: EventLogRawInput = {
-  //   deduplicationId: inputData.deduplicationId,
-  //   version: inputData.version,
-  //   hash: inputData.hash,
-  //   eventName: inputData.eventName,
-  //   type: inputData.type,
-  //   recordingEntity: recordingEntity,
-  //   recordingEntitySearchable: recordingEntitySearchable,
-  //   referencedEntity: referencedEntity,
-  //   referencedEntitySearchable: referencedEntitySearchable,
-  //   data: inputData.data ?? {},
-  // };
-
-  // const isLatest = await isLatestMessage(logData);
-
-  // if (!isLatest.data) {
-  //   res.statusCode = 500;
-  //   return {
-  //     errors: isLatest.errors,
-  //     extensions: isLatest.extensions,
-  //   };
-  // }
-
-  // if (isLatest.data.isLatest === false) {
-  //   // There are logs which are newer than my current version
-  //   // We should assume this log is redundant and should not be processed.
-
-  //   res.statusCode = 200;
-  //   return {
-  //     data: {
-  //       eventLog: {
-  //         deduplicationId: logData.deduplicationId,
-  //       },
-  //     },
-  //   };
-  // }
-
-  // const query = `mutation (
-  //   $where: EventLogWhereUniqueInput!,
-  //   $create: EventLogCreateInput!,
-  //   $update: EventLogUpdateInput!
-  // ) {
-  //   upsertEventLog(
-  //     where: $where,
-  //     upsert: {
-  //       create: $create,
-  //       update: $update
-  //     }
-  //   ) {
-  //     id,
-  //     deduplicationId
-  //   }
-  //   publishEventLog(where:$where) {
-  //     id
-  //   }
-  // }
-  // `;
-
-  // const response = await simpleGraphQL<{ upsertEventLog: EventLogRaw }>(query, {
-  //   where: {
-  //     deduplicationId: logData.deduplicationId,
-  //   },
-  //   create: logData,
-  //   update: logData,
-  // });
-
-  // if (!response.data) {
-  //   res.statusCode = 500;
-  //   return {
-  //     errors: response.errors,
-  //     extensions: response.extensions,
-  //   };
-  // }
-
-  // res.statusCode = 200;
-  // return {
-  //   data: {
-  //     eventLog: {
-  //       deduplicationId: response.data.upsertEventLog.deduplicationId,
-  //     },
-  //   },
-  // };
-};
-
-// function validateDataStuntReview(data) {}
-
-async function isLatestMessage(
-  logData: EventLogRawInput
-): Promise<GraphQLBasic<{ isLatest: boolean }>> {
-  const query = `query($where: EventLogWhereInput!) {
-    eventLogs(where: $where) {
-      deduplicationId
-      version
+    if (!recordingEntityType) {
+      const error = `Recording Entity Type Invalid ${inputData.referencedEntity?._type}`;
+      return makeError(res, 400, error);
     }
-  }`;
 
-  // Get log events which are newer than the current data.
-  const response = await simpleGraphQL<{
-    eventLogs: { deduplicationId: string; version: string }[];
-  }>(query, {
-    where: {
-      deduplicationId: logData.deduplicationId,
-      version_gt: logData.version, // "2022-03-17T04:05:56+00:00",
-    },
-  });
-
-  if (!response.data) {
-    return {
-      errors: response.errors,
-      extensions: response.extensions,
+    recordingEntitySearchable = `${inputData.referencedEntity?._type}:${inputData.referencedEntity?.id}`;
+    recordingEntity = {
+      connect: {
+        [recordingEntityType]: {
+          id: inputData.recordingEntity.id,
+        },
+      },
     };
   }
 
-  if (response.data.eventLogs?.length > 0) {
-    // There are logs which are newer than my current version
-    // We should assume this log is redundant and should not be processed.
-    return { data: { isLatest: false } };
+  // Referenced Entity
+  let referencedEntitySearchable: string | null = null;
+  let referencedEntity: ConnectableEntity | null = null;
+  if (inputData.referencedEntity) {
+    const referencedEntityType =
+      entityTypeMapping[inputData.referencedEntity?._type ?? ""];
+
+    if (!referencedEntityType) {
+      const error = `Referenced Entity Type Invalid ${inputData.referencedEntity?._type}`;
+      return makeError(res, 400, error);
+    }
+
+    referencedEntitySearchable = `${inputData.referencedEntity._type}:${inputData.referencedEntity.id}`;
+    referencedEntity = {
+      connect: {
+        [referencedEntityType]: {
+          id: inputData.referencedEntity.id,
+        },
+      },
+    };
   }
 
-  return { data: { isLatest: true } };
-}
+  const logData: EventLogRawInput = {
+    deduplicationId: inputData.deduplicationId,
+    version: inputData.version,
+    hash: inputData.hash,
+    eventName: inputData.eventName,
+    type: inputData.type,
+    recordingEntity: recordingEntity,
+    recordingEntitySearchable: recordingEntitySearchable,
+    referencedEntity: referencedEntity,
+    referencedEntitySearchable: referencedEntitySearchable,
+    data: inputData.data ?? {},
+  };
+
+  const query = `mutation (
+    $where: EventLogWhereUniqueInput!,
+    $create: EventLogCreateInput!,
+    $update: EventLogUpdateInput!
+  ) {
+    upsertEventLog(
+      where: $where,
+      upsert: {
+        create: $create,
+        update: $update
+      }
+    ) {
+      id,
+      deduplicationId
+    }
+    publishEventLog(where:$where) {
+      id
+    }
+  }
+  `;
+
+  const returnable = await simpleAllGraphQL<EventLogRaw, EventLogPersisted>(
+    "upsertEventLog",
+    query,
+    {
+      where: {
+        deduplicationId: logData.deduplicationId,
+      },
+      create: logData,
+      update: logData,
+    },
+    (eventLog): EventLogPersisted => ({
+      deduplicationId: eventLog.deduplicationId,
+    })
+  );
+
+  res.statusCode = 200;
+  return {
+    ...returnable,
+    data: {
+      eventLog: {
+        deduplicationId: returnable.data?.upsertEventLog[0].deduplicationId,
+      },
+    },
+  };
+};
+
+// function validateDataStuntReview(data) {}
