60cf790eefdf2e263867cc45d65457d1e0d0767a
Dirk Arends <dirk@arends.com.au>
2022-04-09 21:30:42 +1000

    feat: Add VOC Music form

---
+++ src/pages/voc/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6">
+      <v-card>
+        <v-card-title class="text-h2">VOC</v-card-title>
+
+        <v-card-text>Yo Yo Yo. gonna be done tonight</v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" class="py-0"><!-- Force the row reset --></v-col>
+    <v-col cols="12" sm="6">
+      <v-btn block nuxt to="/">Home</v-btn>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <v-btn block color="success" to="/scan">Open scanner</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {
+    this.$setBreadcrumbs([
+      { to: "/", label: "Home" },
+      { to: null, label: "VOC" },
+    ]);
+  },
+};
+</script>
---
+++ src/pages/voc/music.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" class="py-0"><!-- Force the row reset --></v-col>
+      <v-col cols="12" sm="6">
+        <v-btn block nuxt to="/">Home</v-btn>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-btn block color="success" to="/scan">Open scanner</v-btn>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfYIWJkQ8kclbbz2wZ5hQ6ILHXbcbF_Tb8wje9CLjJTlBak8g/viewform?embedded=true"
+          ref="iframe"
+          :width="parentWidth"
+          height="2000"
+          scrolling="no"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading form...
+        </iframe>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      parentWidth: 100,
+    };
+  },
+  mounted() {
+    this.$setBreadcrumbs([
+      { to: "/", label: "Home" },
+      { to: "/voc", label: "VOC" },
+      { to: null, label: "Music" },
+    ]);
+
+    this.fixWidth();
+    setInterval(this.fixWidth, 5000);
+  },
+  methods: {
+    fixWidth() {
+      this.parentWidth = this.$refs.iframe.parentElement.clientWidth;
+    },
+  },
+};
+</script>
