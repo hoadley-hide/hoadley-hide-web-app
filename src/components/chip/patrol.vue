2ea22485c17efda5ab1659e82866c5a619059551
Dirk Arends <dirk@arends.com.au>
2022-04-11 23:29:45 +1000

    feat: Add check in status indicator to patrol lists

--- src/components/chip/patrol.vue
+++ src/components/chip/patrol.vue
@@ -1,7 +1,14 @@
 <template>
-  <span>
+  <span class="d-flex align-center">
     <v-chip small color="pink darken-4">#{{ patrol.patrolNumber }}</v-chip>
-    {{ patrol.name }}
+    <span class="px-2">{{ patrol.name }}</span>
+    <span
+      v-if="$useUser((u) => u._type !== 'patrol')"
+      class="ml-auto text-no-wrap"
+      :class="checkInStatus.colour"
+    >
+      {{ checkInStatus.label }}
+    </span>
   </span>
 </template>
 
@@ -12,6 +19,22 @@ export default {
       type: Object,
       required: true,
       validator: (patrol) => patrol._type === "patrol",
+    },
+  },
+  computed: {
+    checkInStatus() {
+      const status = this.$store.getters["checkpoint/patrolCheckInStatus"](
+        this.patrol,
+        this.$useUser()
+      );
+
+      if (status === "incomplete") {
+        return { colour: "red--text", label: "Not Checked In" };
+      } else if (status === "inflight") {
+        return { colour: "orange--text", label: "Continue" };
+      } else if (status === "complete") {
+        return { colour: "green--text", label: "Checked In" };
+      }
     },
   },
 };
